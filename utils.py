9b29ba8d3a43960587f5ebb8f4360cba88c6d73e
jesongit <188380780@qq.com>
2023-02-17 10:49:53 +0800

    init

--- utils.py
+++ utils.py
@@ -47,7 +47,7 @@ def backup2gd(file: Path, type, to='lemonbk1'):
     try:
         # fclone -v -u copy --transfers {同时上传文件数} --log-file {log_dir} {本地文件} {lemonbk1}:{mv}/{id.7z}
         cmd = f'fclone -v -u copy --transfers={FCLONE_THREAD_CNT} ' \
-              f'--log-file {FCLONE_LOG_FILE} {file.resolve()} {to}:{type}/{file.name}'
+              f'--log-file {FCLONE_LOG_FILE} {file.resolve()} {to}:{type}'
         logging.debug(f'fclone {file.resolve()} type: {type} to: {to}\ncmd: {cmd}')
         ret = subprocess.run(cmd, shell=True)
         logging.debug(f'fclone {file.name} ret: {ret.returncode}')
